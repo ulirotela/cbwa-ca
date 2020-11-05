eedb52271bc684093671a5d1d579f9146813374f
ulirotela <62649611+ulirotela@users.noreply.github.com>
2020-11-05 15:49:58 +0000

    change controllers

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const port = process.env.PORT || 3000;
 const hostname = '0.0.0.0';
 const app = (module.exports = express());
 
-//const users = require('./controllers/users')();
+ const users = require('./controllers/users')();
 
-const hash = require('./models/users')();
+
 const projects = require('./controllers/projects')();
 const issues = require('./controllers/issues')();
 const comments = require('./controllers/comments')(); 
-  
+const hash = require('./models/users')();
 app.use(async (req, res, next) => {
   const FailedAuthMessage = {
     error: 'Failed Authentication',
@@ -42,11 +42,11 @@ app.use(async (req, res, next) => {
 
 app.use(bodyParser.json());
 
-/*users routes
+//users routes
 app.get('/users', users.getController);
 app.get('/users/:email', users.getByEmail);
 app.post('/users', users.postController);
-*/
+
 app.get('/issues', issues.getController);
 app.get('/issues/:issueNumber', issues.getByIssueNumber);
 app.get('/projects/:projectSlug/issues', issues.getByProjectSlug);
